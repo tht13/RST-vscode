c1ef11ba402b538bd359cfa38bc35a0db95197ed
Thomas Haakon Townsend <thomas.haakon@gmail.com>
2016-02-09 21:00:24 +0100

    Non functional code structure implementation

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,40 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import { window, ExtensionContext, commands, TextEditor } from 'vscode';
+import { exec } from 'child_process';
+//TODO: for dev only to see members of vscode, remove for memory optimisation
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "rst-vscode" is now active!');
+export function activate(context: ExtensionContext) {
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+	let disposable = commands.registerCommand('rst.preview', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		window.showInformationMessage('Previewing!');
+        let previewer = new RSTPreviewer(window.activeTextEditor);
+        previewer.preview();
 	});
 
 	context.subscriptions.push(disposable);
+}
+
+class RSTPreviewer {
+    constructor(private editor: TextEditor) {}
+    
+    public preview() {
+        let doc = this.editor.document;
+        let text = doc.getText();
+        let cmd = "python preview.py";
+        exec(cmd, function(error: Error, stdout: ArrayBuffer, stderr: ArrayBuffer) {
+            if (error.toString().length !== 0) {
+                console.warn(`[ERROR] Error message: ${error.toString()}`);
+                console.warn(`[ERROR] Error output: ${stderr.toString()}`);
+            }
+        });
+    }
 }
 
 // this method is called when your extension is deactivated
