15e4edce814802007f53d3c43cc4c1ddfe9109e5
Thomas Haakon Townsend <thomas.haakon@gmail.com>
2016-02-22 18:47:02 +0100

    New attempt at encoding fix

--- src/preview.py
+++ src/preview.py
@@ -4,10 +4,11 @@ filepath = sys.argv[1]
 
 page_string = open(filepath, 'r').read()
 
-overrides = {'input_encoding': 'unicode',
-             'doctitle_xform': True,
-             'initial_header_level': 1,
+# page_string = page_string.encode('utf-8', errors="ignore")
+
+overrides = {'initial_header_level': 1,
              'halt_level': 5}
+
 parts = core.publish_parts(
     source=page_string, source_path=filepath, writer_name='html', settings_overrides=overrides)
 
