ed488d863d6adc1f708a3224c027342748a73e7f
Thomas Haakon Townsend <thomas.haakon@gmail.com>
2016-02-22 18:49:36 +0100

    Log error messages

--- src/extension.ts
+++ src/extension.ts
@@ -60,39 +60,39 @@ export function activate(context: ExtensionContext) {
 export function deactivate() {
 }
 
-    class RstDocumentContentProvider implements TextDocumentContentProvider {
-        private _onDidChange = new EventEmitter<Uri>();
-        private resultText = "";
+class RstDocumentContentProvider implements TextDocumentContentProvider {
+    private _onDidChange = new EventEmitter<Uri>();
+    private resultText = "";
 
-        public provideTextDocumentContent(uri: Uri): string | Thenable<string> {
-            return this.createRstSnippet();
+    public provideTextDocumentContent(uri: Uri): string | Thenable<string> {
+        return this.createRstSnippet();
+    }
+
+    get onDidChange(): Event<Uri> {
+        return this._onDidChange.event;
+    }
+
+    public update(uri: Uri) {
+        this._onDidChange.fire(uri);
+    }
+
+    private createRstSnippet(): string | Thenable<string> {
+        let editor = window.activeTextEditor;
+        if (!(editor.document.languageId === 'rst')) {
+            return this.errorSnippet("Active editor doesn't show a RST document - no properties to preview.")
         }
+        return this.preview(editor);
+    }
 
-        get onDidChange(): Event<Uri> {
-            return this._onDidChange.event;
-        }
-
-        public update(uri: Uri) {
-            this._onDidChange.fire(uri);
-        }
-
-        private createRstSnippet(): string | Thenable<string> {
-            let editor = window.activeTextEditor;
-            if (!(editor.document.languageId === 'rst')) {
-                return this.errorSnippet("Active editor doesn't show a RST document - no properties to preview.")
-            }
-            return this.preview(editor);
-        }
-
-        private errorSnippet(error: string): string {
-            return `
+    private errorSnippet(error: string): string {
+        return `
                 <body>
                     ${error}
                 </body>`;
-        }
+    }
 
-        private buildPage(document: string, headerArgs: string[]): string {
-            return `
+    private buildPage(document: string, headerArgs: string[]): string {
+        return `
             <html lang="en">
             <head>
             ${headerArgs.join('\n')}
@@ -101,64 +101,65 @@ export function deactivate() {
             ${document}
             </body>
             </html>`;
-        }
-
-        private createStylesheet(file: string) {
-            let href = fileUrl(
-                path.join(
-                    __dirname,
-                    "..",
-                    "..",
-                    "src",
-                    "static",
-                    file
-                )
-            )
-            return `<link href="${href}" rel="stylesheet" />`;
-        }
-
-        private fixLinks(document: string, documentPath: string): string {
-            return document.replace(
-                new RegExp("((?:src|href)=[\'\"])(.*?)([\'\"])", "gmi"), (subString: string, p1: string, p2: string, p3: string): string => {
-                    return [
-                        p1,
-                        fileUrl(path.join(
-                            path.dirname(documentPath),
-                            p2
-                        )),
-                        p3
-                    ].join('');
-                }
-            );
-        }
-
-        public preview(editor: TextEditor): Thenable<string> {
-            let doc = editor.document;
-            let promise = new Promise<string>(
-                (resolve, reject) => {
-                    let filepath = doc.fileName
-                    let cmd = "python " + path.join(__dirname, "..", "..", "src", "preview.py") + " " + filepath;
-                    exec(cmd, (error: Error, stdout: Buffer, stderr: Buffer) => {
-                        if (error) {
-                            let errorMessage = [
-                                error.name,
-                                error.message,
-                                error.stack,
-                                '',
-                                stderr.toString()
-                            ].join('\n');
-                            reject(errorMessage);
-                        } else {
-                            let result = this.fixLinks(stdout.toString(), editor.document.fileName);
-                            let headerArgs = [
-                                this.createStylesheet('basic.css'),
-                                this.createStylesheet('default.css')
-                            ];
-                            resolve(this.buildPage(result, headerArgs));
-                        }
-                    });
-                }
-            );
-            return promise
     }
-        }
+
+    private createStylesheet(file: string) {
+        let href = fileUrl(
+            path.join(
+                __dirname,
+                "..",
+                "..",
+                "src",
+                "static",
+                file
+            )
+        )
+        return `<link href="${href}" rel="stylesheet" />`;
+    }
+
+    private fixLinks(document: string, documentPath: string): string {
+        return document.replace(
+            new RegExp("((?:src|href)=[\'\"])(.*?)([\'\"])", "gmi"), (subString: string, p1: string, p2: string, p3: string): string => {
+                return [
+                    p1,
+                    fileUrl(path.join(
+                        path.dirname(documentPath),
+                        p2
+                    )),
+                    p3
+                ].join('');
+            }
+        );
+    }
+
+    public preview(editor: TextEditor): Thenable<string> {
+        let doc = editor.document;
+        let promise = new Promise<string>(
+            (resolve, reject) => {
+                let filepath = doc.fileName
+                let cmd = "python " + path.join(__dirname, "..", "..", "src", "preview.py") + " " + filepath;
+                exec(cmd, (error: Error, stdout: Buffer, stderr: Buffer) => {
+                    if (error) {
+                        let errorMessage = [
+                            error.name,
+                            error.message,
+                            error.stack,
+                            '',
+                            stderr.toString()
+                        ].join('\n');
+                        console.error(errorMessage);
+                        reject(errorMessage);
+                    } else {
+                        let result = this.fixLinks(stdout.toString(), editor.document.fileName);
+                        let headerArgs = [
+                            this.createStylesheet('basic.css'),
+                            this.createStylesheet('default.css')
+                        ];
+                        resolve(this.buildPage(result, headerArgs));
+                    }
+                });
+            }
+        );
+        return promise
+    }
+}
