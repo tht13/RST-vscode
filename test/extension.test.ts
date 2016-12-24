1cb055f95dd3c3af99c462553423b7f7711d52ad
Thomas Townsend <thomas.haakon@gmail.com>
2016-12-24 23:15:22 +0100

    Add tests

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -10,13 +10,45 @@ import * as assert from "assert";
 // as well as import your extension to test it
 import * as vscode from "vscode";
 import * as myExtension from "../src/extension";
+import { RSTDocumentContentProvider } from "../src/document";
+import * as path from "path";
+import * as fs from "fs";
+import { initialize, closeActiveWindows, openFile, samplePath } from './initialize';
 
 // Defines a Mocha test suite to group tests of similar kind together
-// suite("Extension Tests", () => {
+suite("Extension Tests", () => {
+    suiteSetup(done => {
+        initialize().then((a) => {
+            done();
+        }, done);
+    });
 
-//     // Defines a Mocha unit test
-//     test("Something 1", () => {
-//         assert.equal(-1, [1, 2, 3].indexOf(5));
-//         assert.equal(-1, [1, 2, 3].indexOf(0));
-//     });
-// });
+    suiteTeardown(done => {
+        closeActiveWindows().then(done, done);
+    });
+    teardown(done => {
+        closeActiveWindows().then(done, done);
+    });
+
+    // Defines a Mocha unit test
+    test("Example 1 open", done => {
+        const vm = new myExtension.ViewManager();
+        openFile(path.join(samplePath, "example1.rst")).then(editor => {
+            vm.preview(editor.document.uri, false);
+            done();
+        });
+    });
+
+    // Defines a Mocha unit test
+    test("Example 1 to HTML", done => {
+        openFile(path.join(samplePath, "example1.rst")).then(editor => {
+            const provider = new RSTDocumentContentProvider(editor.document);
+            provider.preview().then(val => {
+                fs.readFile(path.join(samplePath, "example1.html"), "utf8", (err, expected) => {
+                    assert.equal(val, expected, "Generated HTML does not match expected");
+                    done();
+                });
+            })
+        });
+    });
+});
